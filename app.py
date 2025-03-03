a6bb2a9f641c4b3e2466dbb0037d361560345826
Jgolo93 <82027225+Jgolo93@users.noreply.github.com>
2025-03-03 06:53:08 +0200

    flask app complete

--- app.py
+++ app.py
@@ -65,4 +65,5 @@ def update(id):
         return render_template('update.html', task=task)  
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8000)
